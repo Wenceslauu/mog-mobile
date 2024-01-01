98334ae28487554a2b7fe44a4e3d9164177ed802
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-12-31 22:55:47 -0300

    feat: create keyboard dismissable view

---
+++ components/DismissKeyboardView.tsx
@@ -0,0 +1,22 @@
+import {
+  Keyboard,
+  TouchableWithoutFeedback,
+  View,
+  ViewStyle,
+} from "react-native";
+
+type DismissKeyboardViewProps = {
+  children: React.ReactNode;
+  style: ViewStyle;
+};
+
+export default function DismissKeyboardView({
+  children,
+  style,
+}: DismissKeyboardViewProps) {
+  return (
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={style}>{children}</View>
+    </TouchableWithoutFeedback>
+  );
+}

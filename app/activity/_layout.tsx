289e95be044da28c4b228218d0b341a6cbba2bc1
Wenceslauu <joaovitor.campagninin@hotmail.com>
2023-12-31 22:08:12 -0300

    feat: instantiate the custom tab navigator in activity screen

---
+++ app/activity/_layout.tsx
@@ -0,0 +1,13 @@
+import CustomTabNavigator from "../../components/CustomTabNavigator";
+
+export default function HomeLayout() {
+  return (
+    <CustomTabNavigator
+      tabs={[
+        { name: "notifications", href: "/activity/notifications" },
+        { name: "requests", href: "/activity/requests" },
+      ]}
+      initialRouteName="notifications"
+    />
+  );
+}

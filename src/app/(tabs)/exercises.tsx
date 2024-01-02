064d3f99a5246a1ac45b1917e1814447b7c71f72
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-01-01 22:32:32 -0300

    feat: list mocked exercises

--- src/app/(tabs)/exercises.tsx
+++ src/app/(tabs)/exercises.tsx
@@ -1,14 +1,39 @@
-import { Pressable, StyleSheet, TextInput, useColorScheme } from "react-native";
+import { Image } from "expo-image";
+import { Pressable, StyleSheet, useColorScheme } from "react-native";
+import { FlashList } from "@shopify/flash-list";
 
 import { Text, View } from "@/components/Themed";
 import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation, Link } from "expo-router";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef } from "react";
 import Colors from "@/constants/Colors";
 
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import DismissKeyboardView from "@/components/DismissKeyboardView";
 import LocalSearchBar from "@/components/LocalSearchBar";
+
+const mockedExercises = [
+  {
+    image: require("../../../assets/images/bench-press.jpg"),
+    name: "Bench Press",
+    isFavorite: false,
+    personalBest: {
+      weight: 225,
+      reps: 5,
+    },
+    targetMuscle: "Chest",
+  },
+  {
+    image: require("../../../assets/images/squat.jpg"),
+    name: "Squat",
+    isFavorite: false,
+    personalBest: {
+      weight: 315,
+      reps: 5,
+    },
+    targetMuscle: "Quads",
+  },
+];
 
 export default function ExercisesScreen() {
   const colorScheme = useColorScheme();
@@ -63,6 +88,11 @@ export default function ExercisesScreen() {
   return (
     <DismissKeyboardView style={styles.container}>
       <LocalSearchBar />
+      <FlashList
+        data={mockedExercises}
+        estimatedItemSize={50}
+        renderItem={({ item }) => <ExerciseCard exercise={item} />}
+      />
       <BottomSheetModal
         ref={bottomSheetModalRef}
         index={1}
@@ -80,14 +110,63 @@ export default function ExercisesScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+type ExerciseCardProps = {
+  exercise: (typeof mockedExercises)[0];
+};
+
+const blurhash =
+  "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+function ExerciseCard({ exercise }: ExerciseCardProps) {
+  return (
+    <View style={exerciseStyles.cardContainer}>
+      <Image
+        style={exerciseStyles.image}
+        source={exercise.image}
+        placeholder={blurhash}
+      />
+      <View>
+        <Text style={exerciseStyles.title}>{exercise.name}</Text>
+        <Text style={exerciseStyles.subtitle}>{exercise.targetMuscle}</Text>
+      </View>
+    </View>
+  );
+}
+
+const exerciseStyles = StyleSheet.create({
+  cardContainer: {
     flex: 1,
+    flexDirection: "row",
     alignItems: "center",
+    justifyContent: "flex-start",
+    borderColor: "white",
+    borderWidth: 1,
+    padding: 5,
+    gap: 10,
+    height: 90,
+  },
+  image: {
+    width: "20%",
+    height: "100%",
+    backgroundColor: "red",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "normal",
+  },
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    height: 1000,
+    gap: 10,
+    paddingTop: 10,
   },
   separator: {
     marginVertical: 30,

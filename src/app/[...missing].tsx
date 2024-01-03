e8c2cd08928ec395ef1801bd383d12fd69c7253e
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-01-02 22:22:40 -0300

    feat: design a way to access the color scheme inside a stylesheet

--- src/app/[...missing].tsx
+++ src/app/[...missing].tsx
@@ -3,6 +3,9 @@ import { StyleSheet, Text, View, useColorScheme } from "react-native";
 import theme from "@/constants/theme";
 
 export default function NotFoundScreen() {
+  const colorScheme = useColorScheme() ?? "light";
+  const styles = createStyles(colorScheme);
+
   return (
     <>
       <Stack.Screen options={{ title: "Oops!" }} />
@@ -17,23 +20,25 @@ export default function NotFoundScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 20,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-  linkText: {
-    fontSize: 14,
-    color: "#2e78b7",
-  },
-});
+const createStyles = (colorScheme: "dark" | "light") => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      padding: 20,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    link: {
+      marginTop: 15,
+      paddingVertical: 15,
+    },
+    linkText: {
+      fontSize: 14,
+      color: theme.colors[colorScheme].tertiary.main,
+    },
+  });
+};

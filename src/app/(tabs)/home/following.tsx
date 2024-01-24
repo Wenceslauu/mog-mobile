77184699bea9177582325b341ea5e3d9149f1c5f
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-01-24 11:03:37 -0300

    feat: list post component instances on following tab

--- src/app/(tabs)/home/following.tsx
+++ src/app/(tabs)/home/following.tsx
@@ -1,17 +1,122 @@
 import Box from "@/components/Box";
-import Text from "@/components/Text";
+import PostCard from "@/components/home/PostCard";
+import { FlashList } from "@shopify/flash-list";
+
+const mockedPosts = [
+  {
+    author: {
+      name: "lui",
+      picture: "https://unavatar.io/github/pedroandrade03",
+    },
+    text: "fazer supino é mt chato affff",
+    duration: "1h",
+    volume: "2500kg",
+    sets: 20,
+    achievements: 2,
+    likes: 10,
+    timestamp: new Date(2024, 0, 17, 9, 30),
+    isLiked: true,
+    images: [],
+    exercises: [
+      {
+        name: "Dumbbell Bench Press",
+        sets: 5,
+      },
+      {
+        name: "Crossover",
+        sets: 3,
+      },
+      {
+        name: "Pushups",
+        sets: 3,
+      },
+      {
+        name: "Dips",
+        sets: 3,
+      },
+      {
+        name: "Chest Press Machine",
+        sets: 3,
+      },
+    ],
+    comments: [
+      {
+        author: {
+          name: "wences",
+          picture: "https://unavatar.io/github/Wenceslauu",
+        },
+        text: "tu ta treinando errado",
+        timestamp: new Date(2024, 0, 17, 20),
+      },
+    ],
+  },
+  {
+    author: {
+      name: "wences",
+      picture: "https://unavatar.io/github/Wenceslauu",
+    },
+    text: "adoro fazer supino",
+    duration: "3h",
+    volume: "3200kg",
+    sets: 30,
+    achievements: 2,
+    likes: 20,
+    timestamp: new Date(2024, 0, 10, 16, 30),
+    isLiked: false,
+    images: [require("../../../../assets/images/bench-press.jpg")],
+    exercises: [
+      {
+        name: "Dumbbell Bench Press",
+        sets: 5,
+      },
+      {
+        name: "Crossover",
+        sets: 3,
+      },
+      {
+        name: "Pushups",
+        sets: 3,
+      },
+      {
+        name: "Dips",
+        sets: 3,
+      },
+      {
+        name: "Chest Press Machine",
+        sets: 3,
+      },
+    ],
+    comments: [
+      {
+        author: {
+          name: "lui",
+          picture: "https://unavatar.io/github/pedroandrade03",
+        },
+        text: "tu ta treinando certo",
+        timestamp: new Date(2024, 0, 10, 20),
+      },
+    ],
+  },
+];
 
 export default function FollowingTab() {
   return (
     <Box
       flex={1}
-      justifyContent="center"
-      alignItems="center"
+      gap="xs"
+      paddingTop="m"
+      paddingHorizontal="l"
       backgroundColor="surface"
     >
-      <Text variant="title" color="onSurface">
-        Following
-      </Text>
+      <FlashList
+        keyboardDismissMode="on-drag"
+        data={mockedPosts}
+        estimatedItemSize={100}
+        renderItem={({ item }) => <PostCard post={item} />}
+        contentContainerStyle={{ paddingBottom: 30 }}
+        ItemSeparatorComponent={() => <Box height={20} />}
+        showsVerticalScrollIndicator={false}
+      />
     </Box>
   );
 }

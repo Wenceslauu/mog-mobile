3418103772a1e90a3cae02e00b088ba800d8ca00
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-01-24 11:02:34 -0300

    feat: create avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,27 @@
+import { Image } from "expo-image";
+
+type AvatarProps = {
+  source: any;
+  size: "s" | "m" | "l";
+};
+
+export default function Avatar({ source, size }: AvatarProps) {
+  const sizeInPixels = AvatarSize[size];
+
+  return (
+    <Image
+      source={source}
+      style={{
+        borderRadius: sizeInPixels / 2,
+        width: sizeInPixels,
+        height: sizeInPixels,
+      }}
+    />
+  );
+}
+
+enum AvatarSize {
+  s = 50,
+  m = 70,
+  l = 100,
+}

306f5a66824bf3dfbaa23f1acd719d000cfc4393
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-01-24 10:59:58 -0300

    chore: create global types for workout log related entities

---
+++ src/types/WorkoutLog.tsx
@@ -0,0 +1,32 @@
+export interface Post {
+  author: {
+    name: string;
+    picture: string;
+  };
+  text: string;
+  duration: string;
+  volume: string;
+  sets: number;
+  likes: number;
+  achievements: number;
+  timestamp: Date;
+  isLiked: boolean;
+  comments: Comment[];
+  exercises: ExerciseLogPreview[];
+  images: any[];
+}
+
+export interface Comment {
+  author: {
+    name: string;
+    picture: string;
+  };
+  text: string;
+  timestamp: Date;
+}
+
+export interface ExerciseLogPreview {
+  image?: any;
+  name: string;
+  sets: number;
+}

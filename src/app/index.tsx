09b638d78279d83612ac31d3a27825f12ef8f5fb
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-02-14 21:13:49 -0300

    fix: home was mounting twice on app start

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,16 @@
-import { Redirect } from "expo-router";
+import { router, useRootNavigationState } from "expo-router";
+import { useEffect } from "react";
 
 export default function IndexScreenPlaceholder() {
-  return <Redirect href="/home/following" />;
+  const rootNavigationState = useRootNavigationState();
+
+  useEffect(() => {
+    // Must ensure root navigator has mounted before trying to navigate
+    if (rootNavigationState?.key) {
+      // <Redirect> was mounting home twice, so we use router.push instead
+      router.push("/home/following");
+    }
+  }, [rootNavigationState]);
+
+  return null;
 }

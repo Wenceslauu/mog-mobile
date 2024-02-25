626c3dc371fa5a83ca25b4500354f27aa01faa96
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-02-25 17:06:39 -0300

    feat: create cycle tab component

---
+++ src/components/routines/CycleTab.tsx
@@ -0,0 +1,30 @@
+import { Workout } from "@/types/Routine";
+import { ScrollView } from "react-native";
+import WorkoutCard from "./WorkoutCard";
+
+type CycleTabProps = {
+  workouts: Workout[];
+};
+
+export default function CycleTab({ workouts }: CycleTabProps) {
+  return (
+    <ScrollView
+      contentContainerStyle={{
+        gap: 16,
+        paddingBottom: 30,
+        paddingHorizontal: 16,
+      }}
+      showsVerticalScrollIndicator={false}
+    >
+      {workouts.map((workout: any, index: number) => {
+        return (
+          <WorkoutCard
+            key={index}
+            workout={workout}
+            isFirst={index === 0 ? true : undefined}
+          />
+        );
+      })}
+    </ScrollView>
+  );
+}

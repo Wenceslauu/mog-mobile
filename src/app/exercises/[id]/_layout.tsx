b07206ffd8657bb0aa392971196b5787f80092df
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-02-26 17:52:51 -0300

    feat: create exercise details dynamic screen

---
+++ src/app/exercises/[id]/_layout.tsx
@@ -0,0 +1,73 @@
+import Box from "@/components/Box";
+import Text from "@/components/Text";
+import CustomTabNavigator from "@/components/CustomTabNavigator";
+import { Theme } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { useTheme } from "@shopify/restyle";
+import { Link, useLocalSearchParams, useNavigation } from "expo-router";
+import { useEffect } from "react";
+import { Pressable } from "react-native";
+
+import ExerciseDetailsAboutTab from "./about";
+import ExerciseDetailsChartsTab from "./charts";
+import Button from "@/components/Button";
+
+export default function ExerciseDetails() {
+  const { name } = useLocalSearchParams();
+  const { colors } = useTheme<Theme>();
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: name,
+      headerRight: () => (
+        <Box flexDirection="row">
+          <Link href="/teste" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="pencil-sharp"
+                  size={25}
+                  color={colors.onSurfaceContainer}
+                  style={{ marginRight: 11, opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+          <Link href="/teste" asChild>
+            <Pressable>
+              {({ pressed }) => (
+                <Ionicons
+                  name="share-outline"
+                  size={25}
+                  color={colors.onSurfaceContainer}
+                  style={{ opacity: pressed ? 0.5 : 1 }}
+                />
+              )}
+            </Pressable>
+          </Link>
+        </Box>
+      ),
+    });
+  }, [navigation, colors]);
+
+  return (
+    <>
+      <CustomTabNavigator
+        tabs={[
+          { name: "about", component: ExerciseDetailsAboutTab },
+          { name: "charts", component: ExerciseDetailsChartsTab },
+        ]}
+        initialRouteName="about"
+      />
+      {/* <Box
+        backgroundColor="surfaceContainer"
+        paddingHorizontal="m"
+        paddingVertical="s"
+        paddingBottom="l"
+      >
+        <Button variant="primary">Start Routine</Button>
+      </Box> */}
+    </>
+  );
+}
---
+++ src/app/exercises/[id]/charts.tsx
@@ -0,0 +1,17 @@
+import Box from "@/components/Box";
+import Text from "@/components/Text";
+
+export default function ExerciseDetailsChartsTab() {
+  return (
+    <Box
+      flex={1}
+      justifyContent="center"
+      alignItems="center"
+      backgroundColor="surface"
+    >
+      <Text variant="title" color="onSurface">
+        Exercise Charts
+      </Text>
+    </Box>
+  );
+}

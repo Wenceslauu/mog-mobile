cab7d94fc7f69cd5484259e24120c7a793cf42ca
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-03-04 16:34:38 -0300

    feat: create achievements section on exercise details charts tab

--- src/app/exercises/[id]/charts.tsx
+++ src/app/exercises/[id]/charts.tsx
@@ -1,17 +1,32 @@
 import Box from "@/components/Box";
+import Table from "@/components/Table";
 import Text from "@/components/Text";
+import { ScrollView } from "react-native";
+
+const mockedAchievements = {
+  "1RM": "100kg",
+  setVolume: "900kg",
+};
 
 export default function ExerciseDetailsChartsTab() {
   return (
-    <Box
-      flex={1}
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="surface"
-    >
-      <Text variant="title" color="onSurface">
-        Exercise Charts
-      </Text>
+    <Box flex={1} gap="xs" backgroundColor="surface">
+      <ScrollView
+        contentContainerStyle={{ gap: 16, paddingBottom: 30 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <Box gap="s" paddingHorizontal="m">
+          <Text variant="title" color="onSurface">
+            Achievements
+          </Text>
+          <Table
+            rows={[
+              { label: "1RM", value: mockedAchievements["1RM"] },
+              { label: "Set Volume", value: mockedAchievements.setVolume },
+            ]}
+          />
+        </Box>
+      </ScrollView>
     </Box>
   );
 }

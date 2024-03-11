983570280fc18832661fa882f44e1d7c622d6c43
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-03-11 11:11:15 -0300

    feat: create routine creation third step screen

--- src/app/create-routine/edit-workout.tsx
+++ src/app/create-routine/edit-workout.tsx
@@ -1,24 +1,58 @@
 import Box from "@/components/Box";
-import Text from "@/components/Text";
+import Button from "@/components/Button";
+import ExerciseCardDraft from "@/components/create-routine/ExerciseCardDraft";
 import { CreateRoutineContext } from "@/contexts/createRoutine";
 import { useLocalSearchParams } from "expo-router";
 import { useContext } from "react";
+import { ScrollView } from "react-native";
 
 export default function EditWorkoutScreen() {
-  const { routine, setRoutine } = useContext(CreateRoutineContext);
-  
-  const { id } = useLocalSearchParams();
+  const { routine, setRoutine, setIsDirty } = useContext(CreateRoutineContext);
+
+  const { cycleIndex, workoutIndex } = useLocalSearchParams();
+
+  const handleAddSet = (exerciseIndex: number) => {
+    const newSet = {
+      reps: 0,
+      intensity: "",
+    };
+
+    setRoutine((draft) => {
+      draft.cycles[Number(cycleIndex)].workouts[Number(workoutIndex)].exercises[
+        exerciseIndex
+      ].sets.push(newSet);
+    });
+
+    setIsDirty(true);
+  };
 
   return (
     <Box
       flex={1}
-      justifyContent="center"
-      alignItems="center"
+      paddingTop="m"
+      paddingHorizontal="m"
       backgroundColor="surface"
     >
-      <Text variant="title" color="onSurface">
-        Create Workout: {id}
-      </Text>
+      <ScrollView
+        contentContainerStyle={{
+          gap: 16,
+          paddingBottom: 30,
+        }}
+        showsVerticalScrollIndicator={false}
+      >
+        {routine.cycles[Number(cycleIndex)].workouts[
+          Number(workoutIndex)
+        ].exercises.map((exerciseDraft, index) => {
+          return (
+            <ExerciseCardDraft
+              key={index}
+              exerciseDraft={exerciseDraft}
+              handleAddSet={() => handleAddSet(index)}
+            />
+          );
+        })}
+        <Button variant="secondary">Add exercise</Button>
+      </ScrollView>
     </Box>
   );
 }

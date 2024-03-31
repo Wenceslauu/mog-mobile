0aa34aeeecacb87826dcfcf36f7804733247ee2e
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-03-31 17:29:41 -0300

    feat: list request cards

--- src/app/activity/requests.tsx
+++ src/app/activity/requests.tsx
@@ -1,17 +1,57 @@
 import Box from "@/components/Box";
-import Text from "@/components/Text";
+import RequestCard from "@/components/activity/RequestCard";
+import { Request, RequestStatus } from "@/types/Activity";
+import { FlashList } from "@shopify/flash-list";
+
+const mockedRequests: Request[] = [
+  {
+    id: 1,
+    requestorUser: {
+      id: 1,
+      name: "Henry",
+      // image: ""
+    },
+    timestamp: new Date(2024, 0, 30, 11),
+    status: RequestStatus.Pending,
+    isRead: false,
+  },
+  {
+    id: 2,
+    requestorUser: {
+      id: 2,
+      name: "Caique",
+      // image: ""
+    },
+    timestamp: new Date(2024, 0, 20, 11),
+    status: RequestStatus.Pending,
+    isRead: false,
+  },
+  {
+    id: 3,
+    requestorUser: {
+      id: 3,
+      name: "Yan",
+      // image: ""
+    },
+    timestamp: new Date(2024, 0, 20, 11),
+    status: RequestStatus.Pending,
+    isRead: true,
+  },
+];
 
 export default function RequestsTab() {
   return (
-    <Box
-      flex={1}
-      justifyContent="center"
-      alignItems="center"
-      backgroundColor="surface"
-    >
-      <Text variant="title" color="onSurface">
-        Requests
-      </Text>
+    <Box flex={1} gap="xs" backgroundColor="surface">
+      <FlashList
+        data={mockedRequests}
+        estimatedItemSize={86}
+        renderItem={({ item }) => <RequestCard request={item} />}
+        contentContainerStyle={{
+          paddingBottom: 30,
+        }}
+        ItemSeparatorComponent={() => <Box height={8} />}
+        showsVerticalScrollIndicator={false}
+      />
     </Box>
   );
 }

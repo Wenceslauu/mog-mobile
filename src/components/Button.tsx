4dc193d4259fe479a085f94dc8dd9cd7e6a9d9f3
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-04-18 11:12:27 -0300

    feat: extend button component to support icons and different sizes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,27 +3,44 @@ import { Pressable } from "react-native";
 import Box from "./Box";
 import Text from "./Text";
 import { ReactNode } from "react";
+import { useTheme } from "@shopify/restyle";
+import { Theme } from "@/constants/theme";
 
 type ButtonProps = {
   children?: ReactNode;
+  icon?: (color: string, size: number) => ReactNode;
   variant: "primary" | "secondary" | "tertiary";
+  size: "s" | "m" | "l";
   onPress?: () => void;
 };
 
-export default function Button({ children, variant, onPress }: ButtonProps) {
+export default function Button({
+  children,
+  icon,
+  variant,
+  size = "m",
+  onPress,
+}: ButtonProps) {
+  const { colors } = useTheme<Theme>();
+
   return (
     <Pressable onPress={onPress}>
       {({ pressed }) => (
         <Box
+          flexDirection="row"
+          gap="xs"
           justifyContent="center"
           alignItems="center"
           backgroundColor={variant === "primary" ? "primary" : "transparent"}
           padding="m"
+          paddingVertical={size === "s" ? "s" : "m"}
           borderRadius="l"
           opacity={pressed ? 0.5 : 1}
           borderWidth={1}
           borderColor={variant === "secondary" ? "primary" : "transparent"}
         >
+          {icon &&
+            icon(variant === "primary" ? colors.onPrimary : colors.primary, 24)}
           <Text
             variant="body"
             color={variant === "primary" ? "onPrimary" : "primary"}

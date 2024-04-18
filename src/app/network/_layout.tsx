bc23c6e1670d87fd6c0083f2fbf11d6b7bdf192f
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-04-18 15:40:05 -0300

    feat: add a search bar on network screens

--- src/app/network/_layout.tsx
+++ src/app/network/_layout.tsx
@@ -2,15 +2,34 @@ import CustomTabNavigator from "@/components/CustomTabNavigator";
 
 import FollowingTab from "./following";
 import FollowersTab from "./followers";
+import Box from "@/components/Box";
+import LocalSearchBar from "@/components/LocalSearchBar";
+import { useState } from "react";
 
 export default function ActivityLayout() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <CustomTabNavigator
-      tabs={[
-        { name: "followers", component: FollowersTab },
-        { name: "following", component: FollowingTab },
-      ]}
-      initialRouteName="followers"
-    />
+    <Box flex={1}>
+      <Box
+        padding="m"
+        style={{
+          paddingBottom: 0,
+        }}
+      >
+        <LocalSearchBar
+          text={searchText}
+          setText={setSearchText}
+          placeholder="Search users"
+        />
+      </Box>
+      <CustomTabNavigator
+        tabs={[
+          { name: "followers", component: FollowersTab },
+          { name: "following", component: FollowingTab },
+        ]}
+        initialRouteName="followers"
+      />
+    </Box>
   );
 }

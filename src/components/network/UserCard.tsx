ab3632bfbfcb4ebfc9b10a793de395b3b6bbdb47
Wenceslauu <joaovitor.campagninin@hotmail.com>
2024-04-18 11:13:04 -0300

    feat: create an user card component

---
+++ src/components/network/UserCard.tsx
@@ -0,0 +1,67 @@
+import { Link } from "expo-router";
+import { Pressable } from "react-native";
+import Box from "../Box";
+import Avatar from "../Avatar";
+import Text from "../Text";
+import { Ionicons } from "@expo/vector-icons";
+import Button from "../Button";
+
+type UserCardProps = {
+  user: {
+    id: number;
+    name: string;
+    picture: string;
+    isFollowed: boolean;
+  };
+};
+
+export default function UserCard({ user }: UserCardProps) {
+  return (
+    <Link
+      href={{
+        pathname: `/profiles/${user.id}`,
+        params: { name: user.name },
+      }}
+      asChild
+    >
+      <Pressable>
+        {({ pressed }) => (
+          <Box
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            backgroundColor="surfaceContainer"
+            paddingVertical="s"
+            paddingHorizontal="m"
+            opacity={pressed ? 0.5 : 1}
+          >
+            <Box flexDirection="row" gap="s">
+              <Avatar source={user.picture} size="m" />
+              <Text variant="body" color="onSurface">
+                {user.name}
+              </Text>
+            </Box>
+            {!user.isFollowed && (
+              <Button
+                variant="secondary"
+                size="s"
+                icon={(color, size) => (
+                  <Ionicons
+                    name="person-add-outline"
+                    color={color}
+                    size={size}
+                  />
+                )}
+                onPress={() => {
+                  console.log("Follow");
+                }}
+              >
+                Follow
+              </Button>
+            )}
+          </Box>
+        )}
+      </Pressable>
+    </Link>
+  );
+}
